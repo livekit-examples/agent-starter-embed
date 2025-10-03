a17be87ad7395a8f6a4a2cb089aae93c6453fc0a
ThomFoolery <thomas.yuill@livekit.io>
2025-10-03 16:40:39 -0400

    design: avatar polish (#42)
    * design: avatar polish
    
    * fix style/token application inside shadow dom
    
    * fix bundled scripts use connection details
    
    * Readme updates
    
    * remove color tokens from app config
    
    also remove `sandboxId` and `agentName` from default appConfig

--- components/livekit/avatar-tile.tsx
+++ components/livekit/avatar-tile.tsx
@@ -1,17 +1,23 @@
 import { type TrackReference, VideoTrack } from '@livekit/components-react';
+import { cn } from '@/lib/utils';
 
 interface AgentAudioTileProps {
   videoTrack: TrackReference;
   className?: string;
 }
 
-export const AvatarTile = ({ videoTrack, className }: AgentAudioTileProps) => {
+export const AvatarTile = ({
+  videoTrack,
+  className,
+  ref,
+}: React.ComponentProps<'video'> & AgentAudioTileProps) => {
   return (
     <VideoTrack
+      ref={ref}
       trackRef={videoTrack}
       width={videoTrack?.publication.dimensions?.width ?? 0}
       height={videoTrack?.publication.dimensions?.height ?? 0}
-      className={className}
+      className={cn(className)}
     />
   );
 };
